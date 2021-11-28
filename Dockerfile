ecc0477e0460c630e8359ca22578c17d8f8c1ea6
Aravind Rajeev <aravindrajeev1@gmail.com>
2021-11-28 20:40:56 +0530

    help and imp

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
-FROM tomcat:jdk8
+FROM tomcat:8.5.47-jdk8-openjdk
+RUN adduser -D tomcat; chown -R tomcat:tomcat /usr/local/tomcat
+USER tomcat
 COPY target/sampleapp.war /usr/local/tomcat/webapps
 CMD ["catalina.sh", "run"]
--- src/main/java/com/ar/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/com/ar/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -2,9 +2,10 @@ package com.ar.dockerjenkinsintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DockerJenkinsIntegrationApplication {
+public class DockerJenkinsIntegrationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
